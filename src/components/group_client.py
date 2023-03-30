8b4b95de9e232de7f03081fdabe7ef02c3e1509b
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-30 11:47:01 +0200

    Avoiding not setting team id

--- src/components/group_client.py
+++ src/components/group_client.py
@@ -20,36 +20,51 @@ class GroupClientComponent:
         self.app.addListBox("tasks_list", self.tasks)
         self.app.addLabelEntry("Description")
         self.app.addButton("Request Help", self.on_request_help)
+        self.app.addImage("light", self.image_path)
         self.init_popup()
         self.app.go()
     
     def init_popup(self):
         # Define the popup window
-        self.app.startSubWindow("Enter Message", modal=True)
+        self.app.startSubWindow("Enter Group Number", modal=True)
         self.app.setSize(300, 200)
         self.app.addLabel("message_label", "Enter your message:")
         self.app.addEntry("message_entry")
         self.app.addButton("Submit", self.submit_message)
+        self.app.setStopFunction(self.sub_window_closed)
         self.app.stopSubWindow()
         
         # Show the popup window
-        self.app.showSubWindow("Enter Message")
+        self.app.showSubWindow("Enter Group Number")
 
     def submit_message(self):
         # Retrieve the message from the input field
         message = self.app.getEntry("message_entry")
-        
+
         # Test if the message is empty
         if message == "":
             self.app.popUp("Error", "Please enter a message", kind="error")
             return
+        
+        # Test if message is a number
+        try:
+            int(message)
+        except ValueError:
+            self.app.popUp("Error", "Please enter a number", kind="error")
+            return
+
         self.group_nr = message
+        self.team_text = "Team " + self.group_nr
         # Close the popup window
-        self.app.hideSubWindow("Enter Message")
+        self.app.hideSubWindow("Enter Group Number")
         # Set the label in the upper right corner
         self.app.setSticky("ne")
-        self.app.setLabel("upper_right_label", self.group_nr)
+        self.app.setLabel("upper_right_label", self.team_text)
     
+    def sub_window_closed(self):
+        # Close the application if the popup window is closed
+        self.app.popUp("Info", "Application will stop", kind="info")
+        self.app.stop()
 
     def on_request_help(self):
         help_request = self.app.getEntry("Description")
