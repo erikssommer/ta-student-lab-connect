c094f29b11b38444c39884729564aa90914dcb60
Erik Storås Sommer <erikssommer@gmail.com>
2023-03-30 17:02:29 +0200

    Added instructions

--- src/components/group_client.py
+++ src/components/group_client.py
@@ -1,34 +1,41 @@
 # Group client component
 import appJar as app
-import os
+from datetime import datetime
 
 class GroupClientComponent:
     def __init__(self):
         self.app = app.gui()
-        self.tasks = ["Task 1", "Task 2", "Task 3"]
+        self.tasks = ["Task 1: Implement a linked list. Duration: 30min", "Task 2: Implement a stack. Duration: 30min", "Task 3: Implement a queue. Duration: 30min", "Task 4: Implement a binary search tree. Duration: 30min", "Task 5: Implement a hash table. Duration: 30min"]
         self.teams = ['Select a team'] + [team for team in range(1, 21)]
         self.image_path = '../../assets/green_light.png'
         self.setup_gui()
 
     def setup_gui(self):
         self.app.setSize(600, 400)  # Set the size of the GUI window
-        self.app.setSticky("ne")
-        self.app.addLabel("upper_right_label", "Group Number")
-        self.app.setSticky("news")
-        self.app.setStretch("both")
-        self.app.addLabel("title", "Group Client Component")
-        self.app.addLabel("tasks_label", "Current Tasks:")
-        self.app.addListBox("tasks_list")
+        self.app.setTitle("Group Client")  # Set the title of the GUI window
+        self.app.addLabel("upper_right_label_date", f"Date: {datetime.now().strftime('%d/%m/%Y')}")
+        self.app.addLabel("upper_right_label", "Team Number")
+        self.app.setLabelSticky("upper_right_label", "ne")
+        self.app.setLabelSticky("upper_right_label_date", "ne")
+        self.app.addLabel("title", "Welcome to the lab").config(font="Helvetica 25")
+        self.app.addLabel("subtitle", "Tasks for this lab session:").config(font="Helvetica 15")
+        self.app.setLabelSticky("subtitle", "w")
+        self.app.addButton("Show Instructions", self.show_message)
+        self.app.setButtonSticky("Show Instructions", "w")
+
         # Add all tasks to the listbox
         for task in self.tasks:
-            self.app.addListItem("tasks_list", task)
             self.app.addCheckBox(task)
 
-        self.app.addLabelEntry("Description")
+        self.app.addLabel("description_label", "Need help? - Ask TAs for help!")
+        self.app.addLabelEntry("Description:")
         self.app.addButton("Request Help", self.on_request_help)
         self.app.addImage("light", self.image_path)
         self.init_popup()
         self.app.go()
+
+    def show_message(self):
+        self.app.infoBox(title="Instructions", message="Use the checkboxes to mark the tasks you have completed. When you are ready to request help, enter a description of the help you need in the text field below and click the 'Request Help' button. The TAs will then come to your group and help you with your tasks. Good luck!")
     
     def init_popup(self):
         # Define the popup window
@@ -72,13 +79,14 @@ class GroupClientComponent:
         self.app.stop()
 
     def on_request_help(self):
-        help_request = self.app.getEntry("Description")
+        help_request = self.app.getEntry("Description:")
         # Test if the help request is empty
         if help_request == "":
             self.app.popUp("Error", "Please enter a description of the help you need", kind="error")
             return
-        self.app.clearEntry("Description")
+        self.app.clearEntry("Description:")
         self.app.popUp("Success", "Help request sent successfully", kind="info")
+        print(help_request)
 
 if __name__ == '__main__':
     client = GroupClientComponent()
